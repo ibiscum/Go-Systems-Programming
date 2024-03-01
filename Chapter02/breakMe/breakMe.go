e9504a6ea65e8b1fae8276f341ea9fbf435fe55f
ibiscum <ulf.luecking@gmail.com>
2024-03-01 20:40:23 +0100

    Ignore.

--- Chapter02/breakMe/breakMe.go
+++ Chapter02/breakMe/breakMe.go
@@ -1,3 +1,5 @@
+//go:build ignore
+
 package main
 
 import "fmt"
@@ -5,7 +7,7 @@ import "fmt"
 func main() {
 	myArray := [4]int{1, 2, 4, -4}
 	threeD := [2][2][2]int{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}
-	fmt.Println("myArray[-1]:", myArray[-1])            //nolint
-	fmt.Println("myArray[10]", myArray[10])             //nolint
-	fmt.Println("threeD[-1][20][0]", threeD[-1][20][0]) //nolint
+	fmt.Println("myArray[-1]:", myArray[-1])
+	fmt.Println("myArray[10]", myArray[10])
+	fmt.Println("threeD[-1][20][0]", threeD[-1][20][0])
 }
